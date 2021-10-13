5cba86d01c90222a2b560d8d9cad6f15d5b075c9
WahajAhmedIqbal <wahaj465@gmail.com>
2021-10-14 03:23:35 +0500

    fff

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import StartingPage from './containers'
+import Starting from './components/Starting'
+
+
+
+const App = () => {
+  return (
+    <>
+      <Starting />
+    </>
+  );
+}
+
+export default App;
